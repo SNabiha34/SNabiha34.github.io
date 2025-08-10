08ec716dd24d0e141d2846813e8213eb646a6a3c
Syeda Nabiha <snabiha34@gmail.com>
2025-08-10 13:02:40 -0400

    Update dom.js
    adding js

--- Portfolio/dom.js
+++ Portfolio/dom.js
@@ -1,1 +1,16 @@
+// Get the slogan element
+const slogan = document.getElementById("sloganText");
+
+// Store the original color
+const originalColor = slogan.style.color || "black";
+
+// When mouse enters -> change color
+slogan.addEventListener("mouseover", function() {
+  slogan.style.color = "tomato"; // change to any color you like
+});
+
+// When mouse leaves -> revert color
+slogan.addEventListener("mouseout", function() {
+  slogan.style.color = originalColor;
+});
 
